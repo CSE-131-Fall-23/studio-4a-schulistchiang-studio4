d2f3c684c66579c2cdb41d6eb901687d7c9c5b1f
Mark Schulist <mschulist2@gmail.com>
2023-10-05 10:35:41 -0500

    more

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,51 +20,85 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); // making Scanner with a File
 
-		String shapeType = in.next();
-		int redComponent = in.nextInt();
-		int greenComponent = in.nextInt();
-		int blueComponent = in.nextInt();
-		boolean isFilled = in.nextBoolean();
-		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		// find total number of shapes
+		int nlines = 0;
+		while (in.hasNextLine()) {
+			nlines++;
+			in.nextLine();
+		}
 		
-		if (shapeType.equals("rectangle")) {
-			double x = in.nextDouble();
-			double y = in.nextDouble();
-			double halfWidth = in.nextDouble();
-			double halfHeight = in.nextDouble();
-			if (isFilled) {
-				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
-			} else {
-				StdDraw.rectangle(x, y, halfWidth, halfHeight);
-			}
-		}
-		else if (shapeType.equals("ellipse")) {
-			double x = in.nextDouble();
-			double y = in.nextDouble();
-			double rad1 = in.nextDouble();
-			double rad2 = in.nextDouble();
-			if (isFilled) {
-				StdDraw.filledEllipse(x, y, rad1, rad2);
-			} else {
-				StdDraw.ellipse(x, y, rad1, rad2);
-			}
-		}
-		else if (shapeType.equals("triangle")) {
-			double x1 = in.nextDouble();
-			double y1 = in.nextDouble();
-			double x2 = in.nextDouble();
-			double y2 = in.nextDouble();
-			double x3 = in.nextDouble();
-			double y3 = in.nextDouble();
-			double[] xCoords = {x1, x2, x3};
-			double[] yCoords = {y1, y2, y3};
-			if (isFilled) {
-				StdDraw.filledPolygon(xCoords, yCoords);
-			} else {
-				StdDraw.polygon(xCoords, yCoords);
+		String shapeType;
+		int redComponent;
+		int greenComponent;
+		int blueComponent;
+		boolean isFilled;
+		
+		double x;
+		double y;
+		double halfWidth;
+		double halfHeight;
+		
+		double rad1;
+		double rad2;
+		
+//		double x1;
+//		double y1;
+//		double x2;
+//		double y2;
+//		double x3;
+//		double y3;
+//		double[] xCoords;
+//		double[] yCoords;
+		
+		// iterate over all of the shapes and add them to StdDraw
+
+		for (int i = 0; i < nlines; i++) {
+
+			shapeType = in.next();
+			redComponent = in.nextInt();
+			greenComponent = in.nextInt();
+			blueComponent = in.nextInt();
+			isFilled = in.nextBoolean();
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+
+			if (shapeType.equals("rectangle")) {
+				x = in.nextDouble();
+				y = in.nextDouble();
+				halfWidth = in.nextDouble();
+				halfHeight = in.nextDouble();
+				if (isFilled) {
+					StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+				} else {
+					StdDraw.rectangle(x, y, halfWidth, halfHeight);
+				}
+			} else if (shapeType.equals("ellipse")) {
+				x = in.nextDouble();
+				y = in.nextDouble();
+				rad1 = in.nextDouble();
+				rad2 = in.nextDouble();
+				if (isFilled) {
+					StdDraw.filledEllipse(x, y, rad1, rad2);
+				} else {
+					StdDraw.ellipse(x, y, rad1, rad2);
+				}
+			} else if (shapeType.equals("triangle")) {
+				double x1 = in.nextDouble();
+				double y1 = in.nextDouble();
+				double x2 = in.nextDouble();
+				double y2 = in.nextDouble();
+				double x3 = in.nextDouble();
+				double y3 = in.nextDouble();
+				double[] xCoords = { x1, x2, x3 };
+				double[] yCoords = { y1, y2, y3 };
+				if (isFilled) {
+					StdDraw.filledPolygon(xCoords, yCoords);
+				} else {
+					StdDraw.polygon(xCoords, yCoords);
+				}
+
 			}
 			
 		}
-		
+
 	}
 }
